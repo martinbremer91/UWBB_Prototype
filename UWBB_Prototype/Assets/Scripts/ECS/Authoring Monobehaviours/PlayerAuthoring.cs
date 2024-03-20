3c0ad0f31b238d37688bfc3b6af6f7585e7c7525
Martin Bremer <martin.bremer91@gmail.com>
2024-03-20 23:26:35 +0100

    added basic player authoring and created basic combat test scene

---
+++ UWBB_Prototype/Assets/Scripts/ECS/Authoring Monobehaviours/PlayerAuthoring.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UWBB.Components;
+using UWBB.GameFramework;
+
+namespace ECS
+{
+    public class PlayerAuthoring : MonoBehaviour
+    {
+        private class PlayerBaker : Baker<PlayerAuthoring>
+        {
+            public override void Bake(PlayerAuthoring authoring)
+            {
+                if (!Application.isPlaying)
+                    return;
+                
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new FactionComponent()
+                {
+                    faction = Faction.Player,
+                });
+                
+                AddComponent(entity, new InvulnerableComponent()
+                {
+                    invulnerable = false,
+                    duration = Main.instance.configs.playerCombatStats.invulnerabilityDuration,
+                    timer = 0
+                });
+            }
+        }
+    }
+}
