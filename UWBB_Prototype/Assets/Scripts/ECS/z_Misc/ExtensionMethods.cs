d2a177227c87a4af3ad9e010a9781dae54baf35c
Martin Bremer <martin.bremer91@gmail.com>
2024-04-07 18:48:10 +0200

    created misc directory, moved PlayerCamera.cs there and created extension methods class with quaternion.Angle extension method

---
+++ UWBB_Prototype/Assets/Scripts/ECS/z_Misc/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+
+namespace ECS
+{
+    public static class ExtensionMethods
+    {
+        public static float Angle(this quaternion a, quaternion b)
+        {
+            float dot = (float)((double)a.value.x * (double)b.value.x + (double)a.value.y * (double)b.value.y +
+                                (double)a.value.z * (double)b.value.z + (double)a.value.w * (double)b.value.w);
+            
+            float num = math.min(math.abs(dot), 1f);
+            return num > 0.9999989867210388 ? 0.0f : (float) (math.acos(num) * 2.0 * 57.295780181884766);
+        }
+    }
+}
